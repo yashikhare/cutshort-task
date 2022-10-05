0f661d1b7975aa18c3c897f29b3453e40b9d276e
yashikhare <yashikhare85@gmail.com>
2022-10-06 00:02:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import Onboarding from './components/Onboarding/Onboarding';
+import Header from './components/Header/Header';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Onboarding />
+    </div>
+  );
+}
+
+export default App;
